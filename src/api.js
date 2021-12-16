5d12a90f8ab721c52deb6dcde29ef3b9e0a1dd6a
Zemola <azeemolatunji@gmail.com>
2021-12-16 07:44:35 +0100

    Added getter function

--- src/api.js
+++ src/api.js
@@ -6,3 +6,16 @@ async function getScores() {
   const data = await response.json();
   return data.result;
 }
+
+async function addScore(obj) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+  const data = await response.json();
+  return data.result;
+}
+module.exports = { getScores, addScore };
